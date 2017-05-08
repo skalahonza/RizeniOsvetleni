1b452357212f8154c6502f2b55d45acf7bda84e9
Jan Skála <skalaja7@fel.cvut.cz>
2017-05-08 12:05:20 +0200

    Move hardware endpoints to separate file.

--- Perifery.h
+++ Perifery.h
@@ -6,16 +6,42 @@
 #define UDPTASK_PERIFERY_H
 
 #include <sys/types.h>
+#include <functional>
+#include <vector>
+#include "HardwareEndpoints.h"
 
 class Perifery {
 public:
     Perifery();
-    ~Perifery();
+
+    void Register_R_Callback(std::function<void(int)> callback);
+
+    void Register_G_Callback(std::function<void(int)> callback);
+
+    void Register_B_Callback(std::function<void(int)> callback);
+
+    void UnRegister_R_Callback(std::function<void(int)> callback);
+
+    void UnRegister_G_Callback(std::function<void(int)> callback);
+
+    void UnRegister_B_Callback(std::function<void(int)> callback);
+
+    void Clear_R_Callbacks();
+
+    void Clear_G_Callbacks();
+
+    void Clear_B_Callbacks();
+
+    virtual ~Perifery();
 
 private:
     void CheckLoop();
     void *map_phys_address(off_t region_base, size_t region_size, int opt_cached);
     char *memdev;
+    volatile bool loop_ = true;
+    std::vector<std::function<void(int)>> R_callbacks_;
+    std::vector<std::function<void(int)>> G_callbacks_;
+    std::vector<std::function<void(int)>> B_callbacks_;
 };
 
 
