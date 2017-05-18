ef1f027e55c0d415f2d25487356fca70cf83c7d2
Jan Skála <skalaja7@fel.cvut.cz>
2017-05-18 14:11:28 +0200

    Add docs.

--- NetTools.h
+++ NetTools.h
@@ -15,10 +15,24 @@
 
 class NetTools {
 public:
+    /**
+     * Checks local IPV4 addres and returns it
+     * @return in standard fromat example: 10.20.30.40
+     */
     static std::string getMyIp();
 
+    /**
+     * Serialize ipv4 addres into uint32
+     * @param ip format n.n.n.n example: 10.20.30.40
+     * @return SERIALIZED Ipv4 ADDRESS
+     */
     static uint32_t toUINT32(std::string ip);
 
+    /**
+     * GEts IPV4 address string from uint32
+     * @param data Serialized uint32 ip address
+     * @return in standard format example: 10.20.30.40
+     */
     static std::string fromUINT32(uint32_t data);
 };
 
